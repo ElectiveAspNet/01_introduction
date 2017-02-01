cee133122645a0e06d4c1e932b4fee6a12a18029
clbokea <clbo@kea.dk>
2017-02-01 16:21:43 +0100

    files and folder rm

--- code/console_examples/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String args[]){
-        RandomChooser r = new RandomChooser();
-        System.out.println("\nThe group to present is: " + r.pickANumber() + "\n");
-    }
-}
